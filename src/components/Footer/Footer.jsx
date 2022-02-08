a5e5ebbd0f5599e5d8e5cea3dffeeef38e48ac11
CoderMohammadShuvo <mohammad.shuvo4099@outlook.com>
2022-02-09 00:39:29 +0600

    all changes done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,57 +6,61 @@ import facebook from "../../assets/icon-social-facebook.svg";
 import telegram from "../../assets/Telegram - Original.svg";
 export default function Footer() {
   return (
-    <div className="footer">
-      <h4>BINART</h4>
-      <div className="footerDetials">
-        <div className="leftSidefooter">
-          <h3>
-            <span style={{ color: "#D16318" }}>Get latest updates</span> <br />{" "}
-            about BinartNFTs
-          </h3>
-          <div className="form">
-            <input type="text" placeholder="your email" />
-            <button>Send</button>
+    <div className="footerMain">
+      <div className="footer">
+        <h4 id="footerBinart">BINART</h4>
+        <div className="footerDetials">
+          <div className="footerDetalsContainer">
+            <div className="leftSidefooter">
+              <h3>
+                <span style={{ color: "#D16318" }}>Get latest updates</span>{" "}
+                <br /> about BinartNFTs
+              </h3>
+              <div className="form">
+                <input type="text" placeholder="your email" />
+                <button>Send</button>
+              </div>
+            </div>
+            <div className="rightSide">
+              <div className="rightsideFirstLine">
+                <a href="">History</a>
+                <a href="" id="footerGallary">
+                  Gallery
+                </a>
+              </div>
+              <div className="rightsideFirstLine">
+                <a href="#slider">Advantages</a>
+                <a href="#faq" id="footerFaq">
+                  FAQ
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="rightSide">
-          <div className="rightsideFirstLine">
-            <a href="">History</a>
-            <a href="" id="footerGallary">
-              Gallery
-            </a>
-          </div>
-          <div className="rightsideFirstLine">
-            <a href="#slider">Advantages</a>
-            <a href="#faq" id="footerFaq">
-              FAQ
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="copyrightAreea">
-        <h5>Ⓒ2022 Binart NFT. All rights reserved</h5>
-        <div className="footersocialIcon">
-          <div className="social">
-            <a href="">
-              <img src={twitter} alt="" />
-            </a>
-          </div>
-          <div className="social">
-            <a href="">
-              <img src={facebook} alt="" />
-            </a>
-          </div>
+        <div className="copyrightAreea">
+          <h5>Ⓒ2022 Binart NFT. All rights reserved</h5>
+          <div className="footersocialIcon">
+            <div className="social">
+              <a href="">
+                <img src={twitter} alt="" />
+              </a>
+            </div>
+            <div className="social">
+              <a href="">
+                <img src={facebook} alt="" />
+              </a>
+            </div>
 
-          <div className="social">
-            <a href="">
-              <img src={discord} alt="" />
-            </a>
-          </div>
-          <div className="social">
-            <a href="">
-              <img src={telegram} alt="" />
-            </a>
+            <div className="social">
+              <a href="">
+                <img src={discord} alt="" />
+              </a>
+            </div>
+            <div className="social">
+              <a href="">
+                <img src={telegram} alt="" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- src/components/WhoWeAre/WhoWeAre.jsx
+++ src/components/WhoWeAre/WhoWeAre.jsx
@@ -4,45 +4,48 @@ import Line from "../../assets/Line 1.svg";
 
 export default function WhoWeAre() {
   return (
-    <div className="whoWeAre">
-      <div className="aboutUs">
-        <div className="aboutTitle">
-          <div className="aboutUsMain">
-            <img src={Line} alt="" />
-            <p>About Us</p>
+    <div className="whoWeareMain">
+      <div className="whoWeAre">
+        <div className="aboutUs">
+          <div className="aboutTitle">
+            <div className="aboutUsMain">
+              <img src={Line} alt="" />
+              <p>About Us</p>
+            </div>
+            <h2>Who We Are</h2>
           </div>
-          <h2>Who We Are</h2>
-        </div>
-        <div className="aboutUsPara">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum
-          </p>
+          <div className="aboutUsPara">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum
+            </p>
+          </div>
         </div>
       </div>
+      <div className="secondNumbers">
+        <div className="numbers">
+          <div className="crafts">
+            <h1>10k</h1>
+            <h4>NFTs Crafted</h4>
+          </div>
 
-      <div className="numbers">
-        <div className="crafts">
-          <h1>10k</h1>
-          <h4>NFTs Crafted</h4>
-        </div>
+          <div className="crafts">
+            <h1>67</h1>
+            <h4>Artists</h4>
+          </div>
 
-        <div className="crafts">
-          <h1>67</h1>
-          <h4>Artists</h4>
-        </div>
+          <div className="crafts">
+            <h1>761</h1>
+            <h4>Collectors</h4>
+          </div>
 
-        <div className="crafts">
-          <h1>761</h1>
-          <h4>Collectors</h4>
-        </div>
-
-        <div className="crafts" style={{ borderRight: "none" }}>
-          <h1>761</h1>
-          <h4>Accessories</h4>
+          <div className="crafts" style={{ borderRight: "none" }}>
+            <h1>761</h1>
+            <h4>Accessories</h4>
+          </div>
         </div>
       </div>
     </div>
